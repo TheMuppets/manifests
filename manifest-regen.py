10a94e717cee2cab42eb1f22ee2ee3acfa78821d
Paul Keith <javelinanddart@gmail.com>
2023-09-04 23:34:38 -0500

    Update script for lineage-20 repos

--- manifest-regen.py
+++ manifest-regen.py
@@ -20,6 +20,8 @@ except KeyError:
 orgName = "TheMuppets"
 org = Github(u, p).get_user(orgName)
 
+branchName = "lineage-20"
+
 file = open("muppets.xml", "w")
 file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 file.write("<manifest>\n")
@@ -28,21 +30,30 @@ repos = []
 
 for repo in org.get_repos():
     if (repo.full_name.find("proprietary_vendor") != -1
-            and repo.full_name.find("proprietary_vendor_xiaomi") == -1):
+            and repo.full_name != ("proprietary_vendor_xiaomi")):
         # Only add repos with the correct branch present
         try:
-            repo.get_branch(branch="lineage-19.1")
+            repo.get_branch(branch=branchName)
             repos.append(repo.full_name)
         except GithubException:
             # Skip the repo, we don't have the branch we want
             continue
 
 for repo in sorted(repos):
-    # Repo name is TheMuppets/proprietary_vendor_$vendor
-    # split on "_" and grab the last one
-    vendor = repo.split("_")[-1]
+    # Repo name is TheMuppets/proprietary_vendor_$vendor_$device
+    splits = repo.split("_")
+    # Some vendor only repos got a lineage-20 branch early
+    if len(splits) < 4:
+         continue
+    vendor = splits[2]
+    device = splits[3]
+    # If there are 4 underscores, the device name contains an underscore
+    if (len(splits) == 5):
+        device += "_" + splits[4]
+
     file.write("  <project name=\"" + repo + "\" path=\"vendor/" + vendor +
-            "\" />\n")
+            "/" + device + "\"" + " revision=\"" + branchName + "\"" +
+            " clone-depth=\"1\"" + " />\n")
 
 file.write("</manifest>\n")
 file.close()
