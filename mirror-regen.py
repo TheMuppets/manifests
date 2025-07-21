54a9bab4daccca4c031a3e901ce8b276cd3a4bff
LuK1337 <priv.luk@gmail.com>
2025-07-21 18:48:03 +0200

    Get rid of GHUSER variable

--- mirror-regen.py
+++ mirror-regen.py
@@ -3,21 +3,21 @@
 import os
 import sys
 try:
+    from github import Auth
     from github import Github
 except ImportError:
     print("Please install the pygithub package via pip3")
     exit(1)
 e = os.environ.copy()
-# Environment variables for github username and api token
+# Environment variable for github api token
 try:
-    u = e["GHUSER"]
-    p = e["GHTOKEN"]
+    token = e["GHTOKEN"]
 except KeyError:
-    print("Please set the GHUSER and GHTOKEN environment variables")
+    print("Please set the GHTOKEN environment variable")
     exit(1)
 
 orgName = "TheMuppets"
-org = Github(u, p).get_user(orgName)
+org = Github(auth=Auth.Token(token)).get_user(orgName)
 
 file = open("default.xml", "w")
 file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
